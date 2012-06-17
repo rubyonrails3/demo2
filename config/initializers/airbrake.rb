03fa61225d49956f4ac019d630846cf3d7c840ba
Ismail Akram <rubyonrails3@gmail.com>
2012-06-17 17:25:17 +0500

    first commit with airbrake setup

---
+++ config/initializers/airbrake.rb
@@ -0,0 +1,3 @@
+Airbrake.configure do |config|
+  config.api_key = '1416fa462d5d8afd47145dd8fba76eeb'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Demo::Application.config.secret_token = 'd03a742a52b83be66f0586a44283261d928a4e189d14370fdb2f4004b2bd07950b12f17ac6a383d121f4a3bcae2a85475de881f16679a3f21f7c62999e111a5a'
